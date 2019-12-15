2657d301011cf0e1a929d0a477a92aac0672735c
Wesley Lopes <wesleyalopes2010@gmail.com>
2019-12-15 11:19:51 -0300

    setup production environment

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,26 @@ require('dotenv').config();
 const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 module.exports = {
+  production: {
+    client: 'pg',
+    connection: {
+      host: DB_HOST,
+      port: DB_PORT,
+      user: DB_USER,
+      database: DB_NAME,
+      password: DB_PASS
+    },
+    migrations: {
+      tableName: 'migrations'
+    },
+    seeds: {
+      directory: `${__dirname}/seeds/production`
+    },
+    pool: {
+      min: 1,
+      max: 20
+    }
+  },
   development: {
     client: 'pg',
     connection: {
